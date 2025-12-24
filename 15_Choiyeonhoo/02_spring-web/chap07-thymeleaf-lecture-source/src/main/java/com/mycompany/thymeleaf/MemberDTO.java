19745c4414d0e440c1eb776c46013d8e9af96a46
cyhhoo <cyhhoo@gmail.com>
2025-12-24 17:04:57 +0900

    251224 수업내용_최연후

---
+++ 15_Choiyeonhoo/02_spring-web/chap07-thymeleaf-lecture-source/src/main/java/com/mycompany/thymeleaf/MemberDTO.java
@@ -0,0 +1,16 @@
+package com.mycompany.thymeleaf;
+
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MemberDTO {
+  private String name;
+  private int age;
+  private char gender;
+  private String address;
+}
